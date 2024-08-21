67fd5c0fe3f9d0cde099765bab7f66cb3cf914ea
leehy0321 <sgn0321@naver.com>
2024-08-21 15:09:34 -0700

    [TEST] add function without download and add cookies and google mail for downloading

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import tempfile
 import os
 import logging
+import requests
 
 from openai import OpenAI
 
@@ -20,21 +21,28 @@ def root():
     return {"message": "Hello World"}
 
 @app.get("/script/")
+async def get_script_from_url_without_download(youtubeURL: str):
+    subtitles = get_video_infomation_from_url(youtubeURL)
+    words_and_timing = transcribe_audio_with_word_time_offsets_without_download(subtitles)
+    final_sentence_and_timing = run_openai_for_making_sentence(words_and_timing)
+    #formatted_json = remove_escape_word_and_restore_json_string(final_sentence_and_timing)
+    return {
+        #"url": url,
+        #"words_and_timing" : words_and_timing,
+        "final_sentence_and_timing" : final_sentence_and_timing,
+        #"formatted_json" : formatted_json,
+        #"kor_sentence_and_timing" : kor_sentence_and_timing
+    }
+
+
+@app.get("/script2/")
 async def get_script_from_url(youtubeURL: str):
     filename = download_audio_with_ytdlp(youtubeURL)
     #filename = "The REAL GENIUS of Steve Jobs.wav"
     words_and_timing = transcribe_audio_with_word_time_offsets(filename)
     cleanup_file(filename)
     final_sentence_and_timing = run_openai_for_making_sentence(words_and_timing)
-
-    # remove escape word and restore json string
-    cleaned_json_str = final_sentence_and_timing.replace("\\n", "\n").replace("\\\"", "\"")
-    cleaned_json_str = cleaned_json_str.strip("\"```json\n").strip("```")
-
-    # pharsing
-    json_data = json.loads(cleaned_json_str)
-
-    formatted_json = json.dumps(json_data, indent=2, ensure_ascii=False)
+    #formatted_json = remove_escape_word_and_restore_json_string(final_sentence_and_timing)
 
     return {
         "message": f"Audio downloaded and saved as {filename}",
@@ -43,6 +51,15 @@ async def get_script_from_url(youtubeURL: str):
         "final_sentence_and_timing" : final_sentence_and_timing,
         #"kor_sentence_and_timing" : kor_sentence_and_timing
     }
+
+def remove_escape_word_and_restore_json_string(origin_data):
+    cleaned_json_str = origin_data.replace("\\n", "\n").replace("\\\"", "\"")
+    cleaned_json_str = cleaned_json_str.strip("\"```json\n").strip("```")
+
+    # pharsing
+    json_data = json.loads(cleaned_json_str)
+
+    return json.dumps(json_data, indent=2, ensure_ascii=False)
 
 def sanitize_filename(url: str) -> str:
     """Sanitize URL to create a valid filename."""
@@ -67,17 +84,17 @@ def download_audio_with_ytdlp(youtube_url: str) -> str:
             'preferredcodec': 'wav',
             'preferredquality': '192',
         }],
-        'cookiesfrombrowser': ('firefox','chrome','vivaldi', 'default', 'BASICTEXT', 'Meta'),
+        #'cookiesfrombrowser': ('firefox','chrome','vivaldi', 'default', 'BASICTEXT', 'Meta'),
         #'verbose': True, # for checking yt-dlp logs
-        'no-cookies': True,
+        'cookies': 'cookies.txt',
+        #'no-cookies': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(youtube_url, download=True)
         file_name = ydl.prepare_filename(info_dict).replace('.webm', '.wav')
-    
-    logger.info(f"Downloading video from URL: {youtube_url} +")
 
+    logger.info(f"Downloading video from URL +")
     return file_name
 
 def transcribe_audio_with_word_time_offsets(audio_file_path):
@@ -133,6 +150,54 @@ def run_openai_for_translating_korean(script):
         {"role": "user", "content": "I will give you few sentences, starting and ending time. Could you translate sentence that is between starting time and ending time to Korean without any other mention that you want to say? And could ou give me during time between start and end time? For example, if you get (Sentence: The greatest people are self-managing. Start: 0.0s, End : 1.72s), you should return (text: The greatest people are self-managing. start: 0.0s, end : 1.72s, dur : 1.72s, kor : 가장 위대한 사람들은 자기 관리를 합니다.). below are the scripts." + script}]
     )
     return completion.choices[0].message.content
+
+def get_video_infomation_from_url(youtube_url: str):
+    logger.info("+ get_video_infomation_from_url : {youtube_url}")
+    try:
+        # Extract video information using yt-dlp
+        ydl_opts = {}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(youtube_url, download=False)
+            tracks = info_dict.get('subtitles', {}).get('en') or info_dict.get('automatic_captions', {}).get('en')
+
+        if tracks and len(tracks) > 0:
+            # Find the English track
+            english_track = tracks[0]
+            subtitles_url = english_track['url']
+
+            # Fetch the subtitles JSON
+            response = requests.get(subtitles_url)
+            response.raise_for_status()
+
+            # Parse the JSON content
+            subtitles_json = response.json()
+            logger.info("get_video_infomation_from_url +")
+            return subtitles_json
+
+    except Exception as e:
+        print('Error downloading subtitles:', str(e))
+        raise  # Re-raise the error to be handled by the caller
+
+def transcribe_audio_with_word_time_offsets_without_download(subtitles):
+    logger.info("+ transcribe_audio_with_word_time_offsets_without_download")
+    events = subtitles.get("events", [])
+    result_sentence = ""
+    for event in events:
+        tStartMs = event.get('tStartMs', 0)
+        dDurationMs = event.get('dDurationMs', 0)
+        segments = event.get('segs', [])
+
+        for segment in segments:
+            word = segment.get('utf8', '')
+            tOffsetMs = segment.get('tOffsetMs', 0)
+            start_time = (tStartMs + tOffsetMs) / 1000.0
+            end_time = (start_time + dDurationMs) / 1000.0
+
+            if word.strip():
+                result_sentence += f"Word: {word}, Start: {start_time}s, End: {end_time}s / "
+
+    logger.info("transcribe_audio_with_word_time_offsets_without_download +")
+    return result_sentence.rstrip(" / ")
 
 def cleanup_file(file_path):
     logger.info(f"+ remove downloaded file: {file_path}")
